7d419e1b017571382fc1d86a4bcc79cd894e956e
zhangxinliang235 <zhangxinliang235@163.com>
2023-03-15 23:34:45 +0800

    初始化、配置全局预设、登录页的设置

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+// scss 全局预设 此处必须有东西，注释也算，不然sass全局不生效
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="login">
+        <ul class="menu-tab">
+        <li :class="{ 'current': item.current }" v-for="( item, index ) in menuTab" :key="index"
+          @click="toggleMenu(item)">{{ item.txt }}
+        </li>
+      </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            menuTab: [
+                {txt: '登录', current: true},
+                {txt: '注册', current: false}
+            ]
+        }
+    },
+    methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(ele => {
+        ele.current = false;
+      })
+      data.current = true
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background: #344a5f;
+  }
+  
+  .login-wrap {
+    width: 330px;
+    margin: auto;
+  }
+  
+  .menu-tab {
+    text-align: center;
+  
+    li {
+      display: inline-block;
+      width: 88px;
+      line-height: 36px;
+      font-size: 14px;
+      border-radius: 2px;
+      color: #fff;
+    }
+  
+    .current {
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+  }
+</style>
