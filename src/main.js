c4890a947de28dccf9e1bf0b68e49f177b17c0b0
zhangxinliang235 <zhangxinliang235@163.com>
2023-03-17 00:14:02 +0800

    使用vue.0升级登录页

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueCompositionAPI from '@vue/composition-api';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI)
+Vue.use(VueCompositionAPI)
+
 Vue.config.productionTip = false;
 
 new Vue({
